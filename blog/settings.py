a20cabe091c4fcd0bd9e3646969f8066811ad634
pawan sapkota <pawansapkota@pawans-MacBook-Air.local>
2024-07-15 12:34:02 +0545

    config

--- blog/settings.py
+++ blog/settings.py
@@ -94,6 +94,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 #     }
 # }
 
+import dj_database_url
+
+DATABASES = {
+	"default": dj_database_url.parse("postgresql://blog:RHe4FH1EXFwCLTaYtJlxlKpSrIAKHWXL@dpg-cqac99jv2p9s73cubnng-a.oregon-postgres.render.com/blog_ovxa")
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
