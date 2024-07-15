4b39112ce667b970703d42b91ae450164829f1b3
Pawan Sapkota <100679210+pawansapkota100@users.noreply.github.com>
2024-07-15 10:31:39 +0545

    conf

--- blog/settings.py
+++ blog/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': '',
-        'PORT': 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('NAME'),
+#         'USER': os.environ.get('USER'),
+#         'PASSWORD': os.environ.get('PASSWORD'),
+#         'HOST': '',
+#         'PORT': 5432
+#     }
+# }
 
 
 # Password validation
